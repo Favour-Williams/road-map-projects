7175a2067d554417e0b283a76e67c7df2ecd2487
Favour Williams <122178439+Favour-Williams@users.noreply.github.com>
2026-03-14 15:28:55 +0200

    Update main.py

--- begginer/ExpenseTracker/main.py
+++ begginer/ExpenseTracker/main.py
@@ -1,1 +1,32 @@
-g
+import argparse
+from services import ExpenseTracker
+
+def main():
+    tracker = ExpenseTracker()
+    try:
+        while True:
+            parser = argparse.ArgumentParser(description="Expense Tracker CLI")
+            parser.add_argument("command", choices=["add", "view", "delete", "update", "exit", "summary"], help="Command to execute")
+            
+            parser.add_argument("--description", type=str, help="Description of the expense")
+            parser.add_argument("--amount", type=float, help="Amount of the expense")
+
+            args = parser.parse_args()
+
+            if args.command == "add":
+                if args.description and args.amount:
+                    print(f"Adding expense: {args.description}, Amount: {args.amount}")
+                    tracker.add_expense(args.amount, args.description)
+                    break
+                else:
+                    print("Please provide description and amount to add an expense.")
+
+            elif args.command == "summary":
+                tracker.expense_summary()
+                break
+            
+    except KeyboardInterrupt:
+        print("\nExiting the Expense Tracker. Goodbye!")
+
+if __name__ == "__main__":
+    main()
